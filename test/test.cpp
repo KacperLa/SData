f3651b6751617a8d915ed85330c527b98aab5e84
KacperLa <klaskak@gmail.com>
2024-04-25 08:26:06 -0400

    updatng tests

--- test/test.cpp
+++ test/test.cpp
@@ -7,6 +7,10 @@
 using namespace sdata;
 using namespace std::chrono;
 
+// make private members public for testing
+#define private public
+
+
 // Mock struct for testing
 struct MockData {
     int value {0};
@@ -42,18 +46,11 @@ TEST_F(SDataTest, MemoryMappedSuccessfully) {
     EXPECT_TRUE(sdata.isMemoryMapped());
 }
 
-// TEST_F(SDataTest, GetData) {
-//     SData<MockData> sdata(temp_file, true);
-//     MockData data;
-//     EXPECT_TRUE(sdata.getData(&data));
-//     // Add your assertions here
-// }
-
-// TEST_F(SDataTest, GetBuffer) {
-//     SData<MockData> sdata(temp_file, 100000000, true);
-//     MockData* buffer = sdata.getBuffer();
-//     // Add your assertions here
-// }
+TEST_F(SDataTest, GetBuffer_not_nullptr) {
+    SData<MockData> sdata(temp_file, 100000000, true);
+    MockData* buffer = sdata.getBuffer();
+    EXPECT_NE(buffer, nullptr);
+}
 
 TEST_F(SDataTest, GetBufferIndex) {
     SData<MockData> sdata(temp_file, 100000000, true);
@@ -63,12 +60,25 @@ TEST_F(SDataTest, GetBufferIndex) {
     EXPECT_EQ(bufferIndex, 1);
 }
 
-// TEST_F(SDataTest, WaitOnStateChange) {
-//     SData<MockData> sdata(temp_file, true);
-//     MockData data;
-//     EXPECT_TRUE(sdata.waitOnStateChange(&data));
-//     // Add your assertions here
-// }
+TEST_F(SDataTest, WaitOnStateChange) {
+    SData<MockData> sdata(temp_file, 150000000, true);
+    MockData data;
+
+    int ret;
+
+    std::thread test_thread([&](){       
+        ret = sdata.waitOnStateChange(&data);
+    });
+
+    // sleep for a while to allow the thread to start
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    sdata.trigger();
+
+    test_thread.join();
+
+    EXPECT_TRUE(ret == 0);
+}
 
 TEST_F(SDataTest, setData_check_BufferIndex) {
     SData<MockData> sdata_producer(temp_file, 100000000, true);
@@ -82,7 +92,6 @@ TEST_F(SDataTest, setData_check_BufferIndex) {
     sdata_producer.setData(&data);
 
     EXPECT_EQ(sdata_producer.getBufferIndex(), 2);
-;
 }
 
 TEST_F(SDataTest, setData_check_BufferIndex_rollover) {
@@ -168,76 +177,79 @@ TEST_F(SDataTest, SetData) {
     EXPECT_EQ(consumer_data.value, producer_data.value);
 }
 
-// TEST_F(SDataTest, speedTest) {
-//     SData<MockData> sdata_producer(temp_file, 100000000, true); // 100ms timeout
-//     SData<MockData> sdata_consumer(temp_file, 100000000, true); // 100ms timeout
+TEST_F(SDataTest, speedTest) {
+    SData<MockData> sdata_producer(temp_file, 150000000, true); // 100ms timeout
+    SData<MockData> sdata_consumer(temp_file, 150000000, true); // 100ms timeout
 
-//     MockData producer_data;
-//     MockData consumer_data;
+    MockData producer_data;
+    MockData consumer_data;
 
-//     int cycles_to_run = 100;
+    int cycles_to_run = 1000;
 
-//     // Counter to count the number of times the consumer has read data
-//     int counter = 0;
+    // statistics
+    int messages_read = 0;
+    int messages_corrupted = 0;
+    std::uint64_t averate_time = 0;
+    std::uint64_t max_time = 0;
+    std::uint64_t min_time = 1000000000;
+    uint64_t time_taken[cycles_to_run];
 
-//     // statistics
-//     std::uint64_t averate_time = 0;
-//     std::uint64_t max_time = 0;
-//     std::uint64_t min_time = 0;
+    // create a thread to read data from the producer
+    std::thread consumer_thread([&](){
+        int ret = nice(1);
+        if (ret == -1) {
+            std::cerr << "Failed to set nice value\n";
+            ASSERT_TRUE(false);
+        }
 
-//     // array to store the time taken to read data
-//     uint64_t time_taken[cycles_to_run];
+        while (consumer_data.value < cycles_to_run-1)
+        {
+            switch (sdata_consumer.waitOnStateChange(&consumer_data))
+            {
+                case 2:
+                    messages_corrupted++;
+                case 1:
+                    break;
+                default:
+                    time_taken[messages_read] = get_time_nano() - consumer_data.timestamp;
+                    messages_read++;
+            }
+        }
+    });
 
-//     // create a thread to read data from the producer
-//     std::thread consumer_thread([&](){
-//         // Get the data from the reader
-//         std::cout << "Consumer data: " << std::to_string(consumer_data.value) << "\n";
-//         while (consumer_data.value < cycles_to_run-1)
-//         {
-//             if (sdata_consumer.waitOnStateChange(&consumer_data) == 1)
-//             {
-//                 break;
-//             }
-//             time_taken[consumer_data.value] = get_time_nano() - consumer_data.timestamp;
-//             counter++;
-//         }
-//         std::cout << "Consumer data: " << std::to_string(consumer_data.value) << "\n";
-//         std::cout << "Consumer thread end \n";
-//     });
+    // Sleep for a while to allow the consumer thread to start
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-//     // Sleep for a while to allow the consumer thread to start
-//     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    for (int i = 0; i < cycles_to_run; i++) {
+        producer_data.value = i;
+        producer_data.timestamp = get_time_nano();
+        sdata_producer.setData(&producer_data);
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
+    }
 
-//     for (int i = 0; i < cycles_to_run; i++) {
-//         producer_data.value = i;
-//         producer_data.timestamp = get_time_nano();
-//         sdata_producer.setData(&producer_data);
-//         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-//     }
+    // Wait for the producer thread to finish
+    consumer_thread.join();
 
-//     // Wait for the producer thread to finish
-//     consumer_thread.join();
+    // Calculate the average time taken to read data
+    std::uint64_t total_time = 0;
+    for (int i = 0; i < messages_read; i++) {
+        if (time_taken[i] > max_time) {
+            max_time = time_taken[i];
+        }
+        if (time_taken[i] < min_time) {
+            min_time = time_taken[i];
+        }
+        total_time += time_taken[i];
+    }
+    averate_time = total_time / cycles_to_run;
 
-//     EXPECT_EQ(counter, cycles_to_run);
-
-//     // Calculate the average time taken to read data
-//     std::uint64_t total_time = 0;
-//     for (int i = 0; i < cycles_to_run; i++) {
-//         if (time_taken[i] > max_time) {
-//             max_time = time_taken[i];
-//         }
-//         if (time_taken[i] < min_time) {
-//             min_time = time_taken[i];
-//         }
-//         total_time += time_taken[i];
-//     }
-//     averate_time = total_time / cycles_to_run;
-
-//     std::cout << "Average Hz: " << 1000000000/averate_time << "\n";
-//     std::cout << "Max time taken to read data: " << max_time << "\n";
-//     std::cout << "Min time taken to read data: " << min_time << "\n";
-// }
-
+    std::cout << "Total messages sent: " << cycles_to_run << "\n";
+    std::cout << "Total messages received: " << messages_read << "\n";
+    std::cout << "Total messages corrupted: " << messages_corrupted << "\n";
+    std::cout << "Average Hz: " << 1000000000/averate_time << "\n";
+    std::cout << "Max time taken to read data: " << max_time/1000000000.0f << "\n";
+    std::cout << "Min time taken to read data: " << min_time/1000000000.0f << "\n";
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
